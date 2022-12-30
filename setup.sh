a7e4851bfa228d3352e244654ed6c27e5ccec163
Saullo Carvalho Castelo Branco <saullocarvalho@gmail.com>
2022-12-30 13:02:16 -0300

    Fix issues in setup.sh

--- setup.sh
+++ setup.sh
@@ -25,8 +25,8 @@ sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=compose
 PHP_ACTOR=repos/phpactor
 git clone https://github.com/phpactor/phpactor $PHP_ACTOR
 cd $PHP_ACTOR
-composer install
-sudo ln -s bin/phpactor /usr/local/bin/phpactor
+composer install --ignore-platform-req=ext-curl --ignore-platform-req=ext-dom --ignore-platform-req=ext-xmlwriter --ignore-platform-req=ext-xml --ignore-platform-req=ext-simplexml
+sudo ln -s $PWD/bin/phpactor /usr/local/bin/phpactor
 cd ../..
 
 # Install pyright
